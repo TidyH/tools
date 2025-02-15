5d78ff32344b7f5e46ce68417cb6fd550490e1ed
TidyH <gwaterman42293@gmail.com>
2025-02-15 14:28:43 -0600

    first linked list problem done

---
+++ leetcode/linked-lists/reverse_linked_list.go
@@ -0,0 +1,64 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type LinkedList struct {
+	Head   *ListNode
+	Length int
+}
+
+func reverseList(head *ListNode) *ListNode {
+	curr := head
+	var prev *ListNode = nil
+
+	for curr != nil {
+		println(head.Val)
+		next := curr.Next // get next node
+		curr.Next = prev  // reverse the pointer
+		prev = curr       // set previous to current node
+		curr = next       // set curr to next node
+	}
+
+	return prev
+}
+
+func main() {
+	println("206. Reverse Linked List")
+
+	node2 := ListNode{
+		Val:  2,
+		Next: nil,
+	}
+
+	node1 := ListNode{
+		Val:  1,
+		Next: &node2,
+	}
+
+	test1 := LinkedList{
+		Head:   &node1,
+		Length: 2,
+	}
+
+	// node3 := ListNode{
+	// 	Val:  3,
+	// 	Next: *node4,
+	// }
+	// node4 := ListNode{
+	// 	Val:  4,
+	// 	Next: nil,
+	// }
+
+	reverseList(test1.Head)
+}
